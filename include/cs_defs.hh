5812728f26296f876c5340aab03030b6ba3899e5
Branden Allen <ballen@cfa.harvard.edu>
2018-02-06 17:21:25 -0500

    Addition of cmake files and basic toolset definitions

---
+++ include/cs_defs.hh
@@ -0,0 +1,11 @@
+/*-------------------------------------------------------------------
+Definitions for the GEANT4-Based Cross Sections Extraction Library
+
+Author: Branden Allen
+Date  : 2018.01.05
+-------------------------------------------------------------------*/
+
+#define MAX_PATH     4095                          //Maximum String Length for Path Names
+#define STRLEN       24                            //Maximum UTC String Length
+#define MAX_ARGS     100                           //Number of Program Input Arguments
+
